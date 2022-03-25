43a0229c0518dff85b54af57a34900157635a67f
Игорь <serpviso@gmail.com>
2022-03-25 22:20:39 +0300

    Добавьте файлы проекта.

---
+++ UTMForms/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace UTMForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Chromium = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Получить статус";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Enabled = false;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Ping",
+            "Загрузка страницы УТМ",
+            "Статус службы"});
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 68);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(165, 49);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(13, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Получить статус службы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(13, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Остановить Transport";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Запустить Transport";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(13, 271);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(165, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Перезагрузка ПК по IP";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 300);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 80);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(12, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(166, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Загрузить страницу утм";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Chromium
+            // 
+            this.Chromium.ActivateBrowserOnCreation = false;
+            this.Chromium.Location = new System.Drawing.Point(184, 12);
+            this.Chromium.Name = "Chromium";
+            this.Chromium.Size = new System.Drawing.Size(604, 426);
+            this.Chromium.TabIndex = 10;
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(13, 387);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(165, 51);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Сброс и проверка другой УТМ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(13, 181);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(164, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Перезагрузить Transport";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.Chromium);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "UTM Fast controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private CefSharp.WinForms.ChromiumWebBrowser Chromium;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ UTMForms/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using CefSharp;
+
+namespace UTMForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Ip = textBox1.Text;
+            ServiceController sc = new ServiceController("Transport", Ip);
+            sc.ServiceName = "Transport";
+            textBox2.Text = Convert.ToString(sc.Status);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            string Ip = textBox1.Text;
+            Ping myPing = new Ping();
+            PingReply reply = myPing.Send(Ip, 2000);
+
+            if (reply.Status == IPStatus.TimedOut)
+            {
+                textBox2.Text = "Данный пк не в сети";
+                button7.Enabled = true;
+
+            }
+            else if(reply.Status == IPStatus.Success)
+            {
+                checkedListBox1.SetItemChecked(0, true);
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                checkedListBox1.Enabled = true;
+
+                ServiceController sc = new ServiceController("Transport", Ip);
+                sc.ServiceName = "Transport";
+                try
+                {                   
+                    textBox2.Text = Convert.ToString(sc.Status);
+                }
+                catch (Exception)
+                {
+                    textBox2.Text = "Данная служба не найдена или не удается получить её статус";
+                }
+                if(Convert.ToString(sc.Status) == "Running")
+                {
+                    checkedListBox1.SetItemChecked(2, true);
+                }
+            }           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Chromium.LoadUrl(textBox1.Text+ ":8080/app/");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            Chromium.LoadUrl("0");
+            textBox1.Enabled = true;
+            textBox1.Text = null;
+            textBox2.Text = null;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            checkedListBox1.SetItemChecked(0, false);
+            checkedListBox1.SetItemChecked(1, false);
+            checkedListBox1.SetItemChecked(2, false);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string Ip = textBox1.Text;
+            ServiceController sc = new ServiceController("Transport", Ip);
+            sc.ServiceName = "Transport";
+            if ((sc.Status.Equals(ServiceControllerStatus.Stopped)) || (sc.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                sc.Start();
+            }
+            else
+            {
+                textBox2.Text = "Служба будет перезапущена через 3 минуты";
+                sc.Stop();
+                Thread.Sleep(180000);
+                sc.Start();
+            }
+            sc.Refresh();
+            textBox2.Text = Convert.ToString(sc.Status);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Ip = textBox1.Text;
+            ServiceController sc = new ServiceController("Transport", Ip);
+            sc.ServiceName = "Transport";
+            sc.Stop();
+            textBox2.Text = "Служба ОСТАНАВЛИВАЕТСЯ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string Ip = textBox1.Text;
+            ServiceController sc = new ServiceController("Transport", Ip);
+            sc.ServiceName = "Transport";
+            sc.Start();
+            textBox2.Text = "Служба ЗАПУСКАЕТСЯ";
+        }
+    }
+}
