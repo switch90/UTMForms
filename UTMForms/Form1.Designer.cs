4c87a85c0d4648b0b9490d8318eac5793e153ee4
Игорь <serpviso@gmail.com>
2022-03-29 12:54:07 +0300

    Добавил иконку

--- UTMForms/Form1.Designer.cs
+++ UTMForms/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.serviceController1 = new System.ServiceProcess.ServiceController();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -169,6 +170,7 @@
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "UTM Fast controller";
             this.Load += new System.EventHandler(this.Form1_Load);
